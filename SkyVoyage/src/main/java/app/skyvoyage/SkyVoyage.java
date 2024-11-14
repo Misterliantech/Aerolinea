1239968bc11d347d68dc9b62fc855c27eb3a808b
JulianchoXX <julianquinterortiz@gmail.com>
2024-11-13 22:29:00 -0500

    Nuevo Proyecto

---
+++ SkyVoyage/src/main/java/app/skyvoyage/SkyVoyage.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package app.skyvoyage;
+
+/**
+ *
+ * @author Quintero Pinto
+ */
+public class SkyVoyage {
+
+    public static void main(String[] args) {
+        PaginaPrincipal.main(args);
+    }
+}
